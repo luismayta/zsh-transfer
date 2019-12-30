d92bff3021975e7bf053a126cc55e539236ec5bd
@slovacus <slovacus@gmail.com>
2019-12-29 19:33:34 -0500

    (#7) refactor: message helper and validation command

--- zsh-transfer.zsh
+++ zsh-transfer.zsh
@@ -8,44 +8,35 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 
-plugin_dir=$(dirname "${0}":A)
-
-# shellcheck source=/dev/null
-source "${plugin_dir}"/src/helpers/messages.zsh
-
-# shellcheck source=/dev/null
-source "${plugin_dir}"/src/helpers/tools.zsh
-
-curl --version 2>&1 > /dev/null
-if [ $? -ne 0 ]; then
-    message_error "Could not find curl."
+if ! type -p curl > /dev/null; then
+    message_warning "Could not find curl."
     return 1
 fi
 
 function transfer {
     if [ $# -eq 0 ];
     then
-        message_error "No arguments specified. Usage:\ntransfer /tmp/file.rst\ncat /tmp/file.rst | transfer file.rst"
+        message_warning "No arguments specified. Usage:\ntransfer /tmp/file.rst\ncat /tmp/file.rst | transfer file.rst"
         return 1
     fi
 
-    tmpfile=$( mktemp -t transferXXX )
-    file=$1
+    tmpfile="$( mktemp -t transferXXX )"
+    file="${1}"
 
     if tty -s;
     then
-        basefile=$(basename "${file}" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
+        basefile="$(basename "${file}" | sed -e 's/[^a-zA-Z0-9._-]/-/g')"
 
         if [ ! -e "${file}" ];
         then
-            message_error "File ${file} doesn't exists."
+            message_warning "File ${file} doesn't exists."
             return 1
         fi
 
         if [ -d "${file}" ];
         then
             zipfile=$( mktemp -t transferXXX.zip )
-            cd $(dirname "${file}") && zip -r -q - $(basename "${file}") >> "${zipfile}"
+            cd "$(dirname "${file}")" && zip -r -q - $(basename "${file}") >> "${zipfile}"
             curl --progress-bar --upload-file "${zipfile}" "https://transfer.sh/${basefile}.zip" >> "${tmpfile}"
             rm -f "${zipfile}"
         else

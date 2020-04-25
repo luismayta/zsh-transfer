323389d1ab4d3fba0914c7aa6d4dc2aabb2b43f4
@slovacus <slovacus@gmail.com>
2020-04-25 14:12:10 -0500

    refactor: implement package prettier settings (#10)

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['@equipindustry/eslint-config'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+};
